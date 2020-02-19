72cdc6115ad66ded09173afc6323638069f554cc
Jo Sprague <josiah.sprague@gmail.com>
2020-02-19 20:12:30 +0000

    Use switch instead of if

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -12,40 +12,40 @@ const initialState: IAppState = {
 };
 
 function rootReducer(state = initialState, action: AnyAction) {
-  if (action.type === SET_STATUS) {
-    return {
-      ...state,
-      status: action.payload,
-      questions: action.payload === IStatus.INIT ? [] : state.questions,
-    };
-  }
-  if (action.type === ADD_QUESTIONS) {
-    return {
-      ...state,
-      questions: (state.questions as IQuestion[]).concat(action.payload),
-    };
-  }
-  if (action.type === SUBMIT_ANSWER) {
-    return {
-      ...state,
-      questions: state.questions.map((q: IQuestion) => {
-        const isCurrentQuestion = q.question === action.payload.question;
-        if (isCurrentQuestion && q.type === 'boolean') {
-          switch (q.type) {
-            case 'boolean':
-              const { correct_answer } = q;
-              const correctBool = /true/i.test(correct_answer);
-              q.is_correct = correctBool === action.payload.answer;
-              break;
-            default:
-              q.is_correct = false; // Answer not verified
+  switch (action.type) {
+    case SET_STATUS:
+      return {
+        ...state,
+        status: action.payload,
+        questions: action.payload === IStatus.INIT ? [] : state.questions,
+      };
+    case ADD_QUESTIONS:
+      return {
+        ...state,
+        questions: (state.questions as IQuestion[]).concat(action.payload),
+      };
+    case SUBMIT_ANSWER:
+      return {
+        ...state,
+        questions: state.questions.map((q: IQuestion) => {
+          const isCurrentQuestion = q.question === action.payload.question;
+          if (isCurrentQuestion && q.type === 'boolean') {
+            switch (q.type) {
+              case 'boolean':
+                const { correct_answer } = q;
+                const correctBool = /true/i.test(correct_answer);
+                q.is_correct = correctBool === action.payload.answer;
+                break;
+              default:
+                q.is_correct = false; // Answer not verified
+            }
           }
-        }
-        return q;
-      }),
-    };
+          return q;
+        }),
+      };
+    default:
+      return state;
   }
-  return state;
 }
 
 export default rootReducer;
