94e1ad46b6714635830f8606a2ec7ae79bd55ae1
Jo Sprague <josiah.sprague@gmail.com>
2020-02-19 19:06:56 +0000

    Updates

---
+++ precache-manifest.5d12a849a4ce71730bdae3df95fdbf32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3bfdeeed13c8aabbf462e1718d9093",
+    "url": "/trivia-demo/index.html"
+  },
+  {
+    "revision": "b0ad71164f059c240375",
+    "url": "/trivia-demo/static/css/main.76268195.chunk.css"
+  },
+  {
+    "revision": "ce2aa16783c6dbc1013a",
+    "url": "/trivia-demo/static/js/2.23b41df9.chunk.js"
+  },
+  {
+    "revision": "2acfdfd0e0f5c1a4a66b84f8031415f8",
+    "url": "/trivia-demo/static/js/2.23b41df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ad71164f059c240375",
+    "url": "/trivia-demo/static/js/main.6133d480.chunk.js"
+  },
+  {
+    "revision": "fb6421a4bc7b090f5388",
+    "url": "/trivia-demo/static/js/runtime-main.3bd628a1.js"
+  }
+]);
